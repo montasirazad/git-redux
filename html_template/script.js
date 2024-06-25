ea0a5e911303f11c8c8b5a5f2ce6f34ebea8467f
Montasir Azad <montasirazad@gmail.com>
2024-06-25 18:38:07 +0600

    added action creator function

--- html_template/script.js
+++ html_template/script.js
@@ -2,15 +2,36 @@ const counterEl = document.getElementById("counter");
 const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
 
+// Action identifier
+
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+
 const initialState = {
   value: 0,
 };
 
+// action creator
+
+const increment = (value) => {
+  return {
+    type: INCREMENT,
+    payload: value,
+  };
+};
+
+const decrement = (value) => {
+  return {
+    type: DECREMENT,
+    payload: value,
+  };
+};
+
 function counterReducer(state = initialState, action) {
-  if (action.type === "increment") {
-    return { ...state, value: state.value + 1 };
-  } else if (action.type === "decrement") {
-    return { ...state, value: state.value - 1 };
+  if (action.type === INCREMENT) {
+    return { ...state, value: state.value + action.payload };
+  } else if (action.type === DECREMENT) {
+    return { ...state, value: state.value - action.payload };
   } else {
     return state;
   }
@@ -23,17 +44,13 @@ const render = () => {
   const state = store.getState();
   counterEl.innerText = state.value.toString();
 };
+render();
 store.subscribe(render);
 
 incrementEl.addEventListener("click", () => {
-  store.dispatch({
-    type: "increment",
-  });
+  store.dispatch(increment(5));
 });
 
 decrementEl.addEventListener("click", () => {
-  store.dispatch({
-    type: "decrement",
-  });
+  store.dispatch(decrement(2));
 });
-
