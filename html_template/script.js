0fd0952fe4466a75b61dd5205df2355cb0338cbd
Montasir Azad <montasirazad@gmail.com>
2024-06-27 02:46:53 +0600

    redux practice

---
+++ html_template/script.js
@@ -0,0 +1,52 @@
+const counterEl = document.getElementById("counter");
+const incrementEl = document.getElementById("increment");
+const decrementEl = document.getElementById("decrement");
+const containerEl = document.getElementById("container");
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+const initialState = {
+  value: 0,
+};
+const increment = (value) => {
+  return {
+    type: INCREMENT,
+    payload: value,
+  };
+};
+
+const decrement = (value) => {
+  return {
+    type: DECREMENT,
+    payload: value,
+  };
+};
+const counterReducer = (state = initialState, action) => {
+  if (action.type === INCREMENT) {
+    return {
+      ...state,
+      value: state.value + action.payload,
+    };
+  } else if (action.type === DECREMENT) {
+    return {
+      ...state,
+      value: state.value - action.payload,
+    };
+  } else {
+    return state;
+  }
+};
+
+const store = Redux.createStore(counterReducer);
+const render = () => {
+  const state = store.getState();
+  counterEl.innerText = state.value.toString();
+};
+render();
+store.subscribe(render);
+incrementEl.addEventListener("click", () => {
+  store.dispatch(increment(5));
+});
+
+decrementEl.addEventListener("click", () => {
+  store.dispatch(decrement(3));
+});
