ac33ea6ebabec09e5e6f87f95b2f026f43a65bbc
Montasir Azad <montasirazad@gmail.com>
2024-06-27 17:28:14 +0600

    created practice branch

--- html_template/script.js
+++ html_template/script.js
@@ -1,8 +1,9 @@
 const counterEl = document.getElementById("counter");
 const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
- 
 const containerEl = document.getElementById("container");
+const addEl = document.getElementById("add");
+
 const INCREMENT = "increment";
 const DECREMENT = "decrement";
 const initialState = {
@@ -50,17 +51,42 @@ incrementEl.addEventListener("click", () => {
 
 decrementEl.addEventListener("click", () => {
   store.dispatch(decrement(3));
-})
-
-let count = 0;
-
-incrementEl.addEventListener("click", () => {
-  count++;
-  counterEl.innerText = count;
 });
 
-decrementEl.addEventListener("click", () => {
-  count--;
-  counterEl.innerText = count;
+addEl.addEventListener("click", function () {
+  const newEl = ` <div class="max-w-md mx-auto mt-10 space-y-5" >
+                <div
+                    class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
+                >
  
+                    <div class="text-2xl font-semibold" id="counter"></div>
+                    <div class="flex space-x-3">
+                        
+ 
+                     
+                    <div class="flex space-x-3">
+                        <button
+                            class="bg-indigo-400 text-white px-3 py-2 rounded shadow" id="increment"
+ 
+                        >
+                            Increment
+                        </button>
+                        <button
+ 
+                        id="decrement"
+                            class="bg-red-400 text-white px-3 py-2 rounded shadow"
+ 
+                            class="bg-red-400 text-white px-3 py-2 rounded shadow" id="decrement"
+
+                        >
+                            Decrement
+                        </button>
+                    </div>
+                </div>
+                
+            </div>
+        </div>`;
+  
+
+  containerEl.innerHTML += newEl;
 });
---
+++ test/script.js
@@ -0,0 +1,94 @@
+const counterEl = document.getElementById("counter");
+const incrementEl = document.getElementById("increment");
+const decrementEl = document.getElementById("decrement");
+const containerEl = document.getElementById("counterContainer");
+const addEl = document.getElementById("add");
+
+// const INCREMENT = "increment";
+// const DECREMENT = "decrement";
+// const initialState = {
+//   value: 0,
+// };
+// const increment = (value) => {
+//   return {
+//     type: INCREMENT,
+//     payload: value,
+//   };
+// };
+
+// const decrement = (value) => {
+//   return {
+//     type: DECREMENT,
+//     payload: value,
+//   };
+// };
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === INCREMENT) {
+//     return {
+//       ...state,
+//       value: state.value + action.payload,
+//     };
+//   } else if (action.type === DECREMENT) {
+//     return {
+//       ...state,
+//       value: state.value - action.payload,
+//     };
+//   } else {
+//     return state;
+//   }
+// };
+
+// const store = Redux.createStore(counterReducer);
+// const render = () => {
+//   const state = store.getState();
+//   counterEl.innerText = state.value.toString();
+// };
+// render();
+// store.subscribe(render);
+// incrementEl.addEventListener("click", () => {
+//   store.dispatch(increment(5));
+// });
+
+// decrementEl.addEventListener("click", () => {
+//   store.dispatch(decrement(3));
+// });
+
+addEl.addEventListener("click", function () {
+    let a=0
+  const newEl = `<div class="max-w-md mx-auto mt-10 space-y-5" >
+                <div
+                    class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
+                >
+ 
+                    <div class="text-2xl font-semibold" id="counter">a=${a+=1}</div>
+                    <div class="flex space-x-3">
+                        
+ 
+                     
+                    <div class="flex space-x-3">
+                        <button
+                            class="bg-indigo-400 text-white px-3 py-2 rounded shadow" id="increment"
+ 
+                        >
+                            Increment
+                        </button>
+                        <button
+ 
+                        id="decrement"
+                            class="bg-red-400 text-white px-3 py-2 rounded shadow"
+ 
+                            class="bg-red-400 text-white px-3 py-2 rounded shadow" id="decrement"
+
+                        >
+                            Decrement
+                        </button>
+                    </div>
+                </div>
+                
+            </div>
+        </div>`;
+
+  let num = 0;
+  ///document.body.innerHTML += newEl
+  counterEl.innerHTML += newEl;
+});
